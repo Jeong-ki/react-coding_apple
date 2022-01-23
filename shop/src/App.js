1e83e926afca36ac4a767ab86e41b53d72d03aa9
Jeong-ki <7863z@naver.com>
2022-01-23 22:29:54 +0900

    shop

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import { Navbar, Container, Nav, NavDropdown, Button } from 'react-bootstrap';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Data from './data.js';
+import Detail from './Detail';
+import axios from 'axios';
+
+import { Link, Route, Switch } from 'react-router-dom';
 
 function App() {
 
   let [shoes, shoes변경] = useState(Data);
+  let [재고, 재고변경] = useState([10, 11, 12]);
 
   return (
     <div className="App">
@@ -15,8 +20,8 @@ function App() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/detail">Detail</Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -29,34 +34,62 @@ function App() {
         </Container>
       </Navbar>
 
-      <div className='jumbo background'>
-        <h1>20% Season Off</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae officia facilis est 
-          voluptatibus laudantium inventore animi eveniet temporibus sequi cumque,
-        </p>
-        <p>
-          <Button variant="primary">Show More</Button>
-        </p>
-      </div>
+      <Switch>
+        <Route exact path="/">
+          <div className='jumbo background'>
+            <h1>20% Season Off</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae officia facilis est 
+              voluptatibus laudantium inventore animi eveniet temporibus sequi cumque,
+            </p>
+            <p>
+              <Button variant="primary">Show More</Button>
+            </p>
+          </div>
 
-      <div className='container'>
-        <div className="row">
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width='100%' alt="신발1" />
-            <h4>상품명</h4>
-            <p>상품설명 & 가격</p>
+          <div className='container'>
+            <div className="row">
+              {
+                shoes.map((shoes, index) => {
+                  return <Card shoes={shoes} index={index} />
+                })
+              }
+            </div>
+            <button className='btn btn-primary' onClick={() => {
+              axios.get('https://codingapple1.github.io/shop/data2.json')
+              .then((result) => { 
+                console.log(result.data);
+                shoes변경([...shoes, ...result.data]);
+              })
+              .catch(() => { 
+                console.log('실패했어요');
+              })
+            }}>더보기</button>
           </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width='100%' alt="신발2" />
-            <h4>상품명</h4>
-            <p>상품설명 & 가격</p>
-          </div><div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width='100%' alt="신발3" />
-            <h4>상품명</h4>
-            <p>상품설명 & 가격</p>
-          </div>
-        </div>
-      </div>
+          
+
+        </Route>
+        <Route path="/detail/:id">
+          <Detail shoes={shoes} 재고={재고} 재고변경={재고변경} />
+        </Route>
+
+        <Route path="/:id">
+          <div>아무거나 적었을 때 이거 보여주셈</div>
+        </Route>
+      </Switch>
+      
+
+    </div>
+  );
+}
+
+
+
+function Card({shoes, index}) {
+  return (
+    <div className="col-md-4">
+      <img src={`https://codingapple1.github.io/shop/shoes${index+1}.jpg`} width='100%' alt="신발" />
+      <h4>{shoes.title}</h4>
+      <p>{shoes.content} & {shoes.price}</p>
     </div>
   );
 }
---
+++ shop/src/Detail.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import './Detail.scss';
+
+let 박스 = styled.div`
+  padding: 20px;
+`;
+
+let 제목 = styled.h4`
+  font-size: 25px;
+  color: ${ props => props.색상};
+`;
+
+function Detail(props) {
+  let [alert, setAlert] = useState(true);
+  let [inputData, inputData변경] = useState('');
+
+  useEffect(() => {
+    let 타이머 = setTimeout(() => { setAlert(false); }, 2000);
+    return () => { clearTimeout(타이머) }
+  }, [alert]);
+
+  let { id } = useParams();
+  let history = useHistory();
+  let 찾은상품 = props.shoes.find((상품) => {return 상품.id == id})
+
+  return (
+        <div className="container">
+          <박스>
+            <제목 색상='green'>Detail</제목>
+            <제목 className='red'>Detail</제목>
+          </박스>
+
+          { inputData }
+          <input onChange={(e) => { inputData변경(e.target.value) }} />
+
+          { alert 
+            ? (<div className="my-alert2">
+                <p>재고가 얼마 남지 않았습니다.</p>
+              </div>)
+            : null
+          }
+          <div className="row">
+            <div className="col-md-6">
+              <img src={`https://codingapple1.github.io/shop/shoes${찾은상품.id + 1}.jpg`} width="100%" />
+            </div>
+            <div className="col-md-6 mt-4">
+              <h4 className="pt-5">{찾은상품.title}</h4>
+              <p>{찾은상품.content}</p>
+              <p>{찾은상품.price}</p>
+
+              <Info 재고={props.재고} />
+
+              <button className="btn btn-danger" onClick={() => {props.재고변경([9, 11, 12])}}>주문하기</button> 
+              <button className="btn btn-danger" onClick={() => {
+                // history.goBack();
+                history.push('/');
+              }}>뒤로가기</button> 
+            </div>
+          </div>
+        </div> 
+  )
+}
+
+function Info(props) {
+  return (
+    <p>재고: {props.재고[0]}</p>
+  )
+}
+
+export default Detail;
